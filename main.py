5d394b58bd0db0b1d17fc013f2275799a1c3d925
Yeabsira Tesfaye <yeabsiramersha%*@gmail.com>
2023-02-14 22:41:14 +0300

    succesfully commited

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello world') nn
+weight = input('Your Weight: ')
+unit = input('(L)bs or (K)g: ').lower()
+if unit == 'l':
+    weight_new = float(weight)*0.45
+    new_unit = 'Kg'
+else:
+    weight_new = float(weight)*2.2
+    new_unit = 'Lbs'
+print(f'You are {weight_new} {new_unit}')
